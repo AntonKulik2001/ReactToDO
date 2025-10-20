dc253e88701d72458db8d4bdca5d353c1ccc6145
Antonkulik2001 <antonkulik201@gmail.com>
2025-10-20 21:56:05 +0300

    code correct

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import { ToDoList } from './pages/ToDoList';
+import './assets/scss/style.scss';
+import './assets/scss/normalize.scss';
+import { ToDoListPage } from './pages/ToDoListPage';
+import { Header } from './components/Header/Header';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoList />
+    <Header />
+    <ToDoListPage />
   </React.StrictMode>
 );
 
---
+++ src/pages/ToDoListPage.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Form } from '../components/Form/Form';
+import { ToDoList } from "../components/ToDoList/ToDoList";
+
+
+export const ToDoListPage = () => {
+    return (
+        <>
+            <Form />
+            <ToDoList />
+        </>
+    )
+}
